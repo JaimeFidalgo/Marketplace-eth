3d07ea7c7916b69cbb66812a09313425cad74c78
JaimeFidalgo <jaimefidalgosantos@gmail.com>
2022-06-21 18:14:48 +0200

    common navbar and footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "efaad458d7bd669b"; }
+/******/ 		__webpack_require__.h = function() { return "6c22f9e56f8a343b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
